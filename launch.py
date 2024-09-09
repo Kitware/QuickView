248f579cf71354ee0a1670dbd73d03ba130d56f4
Abhishek Yenpure <abhishek.yenpure@hotmail.com>
2024-09-09 13:08:19 -0700

    Adding updates

--- launch.py
+++ launch.py
@@ -5,15 +5,15 @@ import traceback
 from subprocess import run
 
 def setup_env():
+    workdir = os.path.abspath(os.path.dirname(__file__))
+    print("Workdir : ", workdir)
+    env     = os.path.join(workdir, ".pvenv")
     try:
-        workdir = os.path.abspath(os.path.dirname(__file__))
-        print("Workdir : ", workdir)
-        env     = os.path.join(workdir, ".pvenv")
         if os.path.exists(env):
             return True
         else:
             venv.create(env, with_pip=True)
-            run(["bin/pip", "install", workdir], cwd=env)
+            run(["bin/pip", "install", "--prefer-binary", workdir], cwd=env)
     except Exception as e:
         print("Error occurred while trying to access environment ", e)
         traceback.print_exc()
