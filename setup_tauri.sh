3f9d89bf26fe12614a2252c2537a4e27905540df
Abhishek Yenpure <abhishek.yenpure@hotmail.com>
2025-05-16 17:06:10 -0700

    Adding tauri bundling scripts for QuickView

---
+++ setup_tauri.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+set -x
+
+pip install .
+
+python -m PyInstaller --clean --noconfirm \
+        --distpath src-tauri \
+        --name server --hidden-import pkgutil \
+        --collect-data trame_vtk \
+        --collect-data trame_client \
+        --collect-data trame_vuetify \
+        --collect-data paraview \
+        --collect-data quickview \
+        --hidden-import pkgutil \
+        --add-binary="$(which pvpython):."  \
+        quickview/app.py
+
+python -m trame.tools.www --output ./src-tauri/www
