2a97c156ec578b22d8c7c1a965d05dc084f212aa
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2025-10-20 14:02:28 -0600

    fix(logo): use same logo

--- src/e3sm_quickview/assets/__init__.py
+++ src/e3sm_quickview/assets/__init__.py
@@ -1,5 +1,5 @@
 from trame.assets.local import LocalFileManager
 
 ASSETS = LocalFileManager(__file__)
-ASSETS.url("icon", "small-icon.png")
+ASSETS.url("icon", "icon-192.png")
 ASSETS.url("banner", "banner.jpg")
