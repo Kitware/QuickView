8dbaf0033b8b4e71eebb8f2918575102711ca50d
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2025-10-22 15:50:41 -0600

    fix(color_range): support scientific notation

--- src/e3sm_quickview/view_manager2.py
+++ src/e3sm_quickview/view_manager2.py
@@ -9,8 +9,8 @@ from trame_dataclass.core import StateDataModel
 
 from paraview import simple
 
-from e3sm_quickview.utils.color import get_cached_colorbar_image
-from e3sm_quickview.utils.color import COLORBAR_CACHE
+from e3sm_quickview.components import view
+from e3sm_quickview.utils.color import get_cached_colorbar_image, COLORBAR_CACHE
 from e3sm_quickview.presets import COLOR_BLIND_SAFE
 
 
@@ -55,6 +55,10 @@ class ViewConfiguration(StateDataModel):
     invert: bool = False
     color_blind: bool = False
     use_log_scale: bool = False
+    color_value_min: str = "0"
+    color_value_max: str = "1"
+    color_value_min_valid: bool = True
+    color_value_max_valid: bool = True
     color_range: list[float] = (0, 1)
     override_range: bool = False
     order: int = 0
@@ -117,6 +121,10 @@ class VariableView(TrameComponent):
 
         # Reactive behavior
         self.config.watch(
+            ["color_value_min", "color_value_max"],
+            self.color_range_str_to_float,
+        )
+        self.config.watch(
             ["override_range", "color_range"], self.update_color_range, eager=True
         )
         self.config.watch(
@@ -158,12 +166,36 @@ class VariableView(TrameComponent):
             self.lut.UseLogScale = 1
         self.render()
 
+    def color_range_str_to_float(self, color_value_min, color_value_max):
+        try:
+            min_value = float(color_value_min)
+            self.config.color_value_min_valid = not math.isnan(min_value)
+        except ValueError:
+            self.config.color_value_min_valid = False
+
+        try:
+            max_value = float(color_value_max)
+            self.config.color_value_max_valid = not math.isnan(max_value)
+        except ValueError:
+            self.config.color_value_max_valid = False
+
+        if self.config.color_value_min_valid and self.config.color_value_max_valid:
+            self.config.color_range = [min_value, max_value]
+
     def update_color_range(self, *_):
         if self.config.override_range:
-            if math.isnan(self.config.color_range[0]) or math.isnan(
-                self.config.color_range[1]
-            ):
+            skip_update = False
+            if math.isnan(self.config.color_range[0]):
+                skip_update = True
+                self.config.color_value_min_valid = False
+
+            if math.isnan(self.config.color_range[1]):
+                skip_update = True
+                self.config.color_value_max_valid = False
+
+            if skip_update:
                 return
+
             self.lut.RescaleTransferFunction(*self.config.color_range)
         else:
             self.representation.RescaleTransferFunctionToDataRange(False, True)
@@ -175,6 +207,10 @@ class VariableView(TrameComponent):
             if data_array:
                 data_range = data_array.GetRange()
                 self.config.color_range = data_range
+                self.config.color_value_min = str(data_range[0])
+                self.config.color_value_max = str(data_range[1])
+                self.config.color_value_min_valid = True
+                self.config.color_value_max_valid = True
                 self.lut.RescaleTransferFunction(*data_range)
         self.render()
 
@@ -194,108 +230,7 @@ class VariableView(TrameComponent):
                     dense=True,
                     classes="ma-0 pa-0 bg-black opacity-90 d-flex align-center",
                 ):
-                    with v3.VBtn(
-                        icon=True,
-                        density="compact",
-                        variant="plain",
-                        classes="mx-1",
-                        size="small",
-                    ):
-                        v3.VIcon(
-                            "mdi-arrow-expand",
-                            size="x-small",
-                            style="transform: scale(-1, 1);",
-                        )
-                        with v3.VMenu(activator="parent"):
-                            with self.config.provide_as("config"):
-                                with v3.VList(density="compact"):
-                                    v3.VListItem(
-                                        subtitle="Full Screen",
-                                        click=f"active_layout = '{self.name}'",
-                                    )
-                                    v3.VDivider()
-                                    with v3.VListItem(
-                                        subtitle="Line Break",
-                                        click="config.break_row = !config.break_row",
-                                    ):
-                                        with v3.Template(v_slot_append=True):
-                                            v3.VSwitch(
-                                                v_model="config.break_row",
-                                                hide_details=True,
-                                                density="compact",
-                                                color="primary",
-                                            )
-                                    with v3.VListItem(subtitle="Offset"):
-                                        v3.VBtn(
-                                            "0",
-                                            classes="text-none ml-2",
-                                            size="small",
-                                            variant="outined",
-                                            click="config.offset = 0",
-                                            active=("config.offset === 0",),
-                                        )
-                                        v3.VBtn(
-                                            "1",
-                                            classes="text-none ml-2",
-                                            size="small",
-                                            variant="outined",
-                                            click="config.offset = 1",
-                                            active=("config.offset === 1",),
-                                        )
-                                        v3.VBtn(
-                                            "2",
-                                            classes="text-none ml-2",
-                                            size="small",
-                                            variant="outined",
-                                            click="config.offset = 2",
-                                            active=("config.offset === 2",),
-                                        )
-                                        v3.VBtn(
-                                            "3",
-                                            classes="text-none ml-2",
-                                            size="small",
-                                            variant="outined",
-                                            click="config.offset = 3",
-                                            active=("config.offset === 3",),
-                                        )
-                                        v3.VBtn(
-                                            "4",
-                                            classes="text-none ml-2",
-                                            size="small",
-                                            variant="outined",
-                                            click="config.offset = 4",
-                                            active=("config.offset === 4",),
-                                        )
-                                        v3.VBtn(
-                                            "5",
-                                            classes="text-none ml-2",
-                                            size="small",
-                                            variant="outined",
-                                            click="config.offset = 5",
-                                            active=("config.offset === 5",),
-                                        )
-                                    v3.VDivider()
-
-                                    v3.VListItem(
-                                        subtitle="Full width",
-                                        click="active_layout = 'auto_layout';config.size = 12",
-                                    )
-                                    v3.VListItem(
-                                        subtitle="1/2 width",
-                                        click="active_layout = 'auto_layout';config.size = 6",
-                                    )
-                                    v3.VListItem(
-                                        subtitle="1/3 width",
-                                        click="active_layout = 'auto_layout';config.size = 4",
-                                    )
-                                    v3.VListItem(
-                                        subtitle="1/4 width",
-                                        click="active_layout = 'auto_layout';config.size = 3",
-                                    )
-                                    v3.VListItem(
-                                        subtitle="1/6 width",
-                                        click="active_layout = 'auto_layout';config.size = 2",
-                                    )
+                    view.create_size_menu(self.name, self.config)
                     with html.Div(
                         self.variable_name,
                         classes="text-subtitle-2 pr-2",
@@ -361,150 +296,7 @@ class VariableView(TrameComponent):
                         self.view, interactive_ratio=1, ctx_name=self.name
                     )
 
-                with self.config.provide_as("config"):
-                    with html.Div(
-                        classes="bg-blue-grey-darken-2 d-flex align-center",
-                        style="height:1rem;position:relative;top:0;user-select:none;cursor:context-menu;",
-                    ):
-                        with v3.VMenu(
-                            v_model="config.menu",
-                            activator="parent",
-                            location=(
-                                "active_layout !== 'auto_layout' || config.size == 12 ? 'top' : 'end'",
-                            ),
-                            close_on_content_click=False,
-                        ):
-                            with v3.VCard(style="max-width: 360px;"):
-                                with v3.VCardItem(classes="pb-0"):
-                                    v3.VIconBtn(
-                                        raw_attrs=[
-                                            '''v-tooltip:bottom="config.color_blind ? 'Colorblind safe presets' : 'All color presets'"'''
-                                        ],
-                                        icon=(
-                                            "config.color_blind ? 'mdi-shield-check-outline' : 'mdi-palette'",
-                                        ),
-                                        click="config.color_blind = !config.color_blind",
-                                        size="small",
-                                        text="Colorblind safe",
-                                        variant="text",
-                                    )
-                                    v3.VIconBtn(
-                                        raw_attrs=[
-                                            '''v-tooltip:bottom="config.invert ? 'Inverted preset' : 'Normal preset'"'''
-                                        ],
-                                        icon=(
-                                            "config.invert ? 'mdi-invert-colors' : 'mdi-invert-colors-off'",
-                                        ),
-                                        click="config.invert = !config.invert",
-                                        size="small",
-                                        text="Invert",
-                                        variant="text",
-                                    )
-                                    v3.VIconBtn(
-                                        raw_attrs=[
-                                            '''v-tooltip:bottom="config.use_log_scale ? 'Use log scale' : 'Use linear scale'"'''
-                                        ],
-                                        icon=(
-                                            "config.use_log_scale ? 'mdi-math-log' : 'mdi-stairs'",
-                                        ),
-                                        click="config.use_log_scale = !config.use_log_scale",
-                                        size="small",
-                                        text=(
-                                            "config.use_log_scale ? 'Log scale' : 'Linear scale'",
-                                        ),
-                                        variant="text",
-                                    )
-                                    v3.VIconBtn(
-                                        raw_attrs=[
-                                            '''v-tooltip:bottom="config.override_range ? 'Use custom range' : 'Use data range'"'''
-                                        ],
-                                        icon=(
-                                            "config.override_range ? 'mdi-arrow-expand-horizontal' : 'mdi-pencil'",
-                                        ),
-                                        click="config.override_range = !config.override_range",
-                                        size="small",
-                                        text="Use data range",
-                                        variant="text",
-                                    )
-
-                                    with v3.Template(v_slot_append=True):
-                                        v3.VLabel(
-                                            "{{ config.preset }}",
-                                            classes="mr-2 text-caption",
-                                        )
-                                        v3.VIconBtn(
-                                            icon="mdi-close",
-                                            size="small",
-                                            text="Close",
-                                            click="config.menu=false",
-                                        )
-                                with v3.VCardItem(
-                                    v_show="config.override_range", classes="py-0"
-                                ):
-                                    v3.VNumberInput(
-                                        model_value=("config.color_range[0]",),
-                                        update_modelValue="config.color_range = [Number($event), config.color_range[1]]",
-                                        hide_details=True,
-                                        density="compact",
-                                        variant="outlined",
-                                        flat=True,
-                                        label="Min",
-                                        classes="mt-2",
-                                        control_variant="hidden",
-                                        precision=("15",),
-                                        step=(
-                                            "Math.max(0.0001, (config.color_range[1] - config.color_range[0]) / 255)",
-                                        ),
-                                    )
-                                    v3.VNumberInput(
-                                        model_value=("config.color_range[1]",),
-                                        update_modelValue="config.color_range = [config.color_range[0], Number($event)]",
-                                        hide_details=True,
-                                        density="compact",
-                                        variant="outlined",
-                                        flat=True,
-                                        label="Max",
-                                        classes="mt-2",
-                                        control_variant="hidden",
-                                        precision=("15",),
-                                        step=(
-                                            "Math.max(0.0001, (config.color_range[1] - config.color_range[0]) / 255)",
-                                        ),
-                                    )
-                                v3.VDivider(classes="mt-2")
-                                with v3.VList(density="compact", max_height="40vh"):
-                                    with v3.VListItem(
-                                        v_for="url, name in (config.invert ? luts_inverted : luts_normal)",
-                                        v_show="!config.color_blind || safe_color[name]",
-                                        key="name",
-                                        subtitle=("name",),
-                                        click=(
-                                            self.update_color_preset,
-                                            "[name, config.invert, config.use_log_scale]",
-                                        ),
-                                        active=("config.preset === name",),
-                                    ):
-                                        html.Img(
-                                            src=("url",),
-                                            style="width:100%;min-width:20rem;height:1rem;",
-                                            classes="rounded",
-                                        )
-                        html.Div(
-                            "{{ utils.quickview.formatRange(config.color_range?.[0], config.use_log_scale) }}",
-                            classes="text-caption px-2 text-no-wrap",
-                        )
-                        with html.Div(
-                            classes="overflow-hidden rounded w-100", style="height:70%;"
-                        ):
-                            html.Img(
-                                src=("config.preset_img",),
-                                style="width:100%;height:2rem;",
-                                draggable=False,
-                            )
-                        html.Div(
-                            "{{ utils.quickview.formatRange(config.color_range?.[1], config.use_log_scale) }}",
-                            classes="text-caption px-2 text-no-wrap",
-                        )
+                view.create_bottom_bar(self.config, self.update_color_preset)
 
 
 class ViewManager(TrameComponent):
@@ -576,6 +368,8 @@ class ViewManager(TrameComponent):
         config_b = self._active_configs[variable_b]
         config_a.order, config_b.order = config_b.order, config_a.order
         config_a.size, config_b.size = config_b.size, config_a.size
+        config_a.offset, config_b.offset = config_b.offset, config_a.offset
+        config_a.break_row, config_b.break_row = config_b.break_row, config_a.break_row
 
     def apply_size(self, n_cols):
         if not self._last_vars:
