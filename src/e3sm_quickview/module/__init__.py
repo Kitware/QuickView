4e59361ca904177aa3408bd480d0917f788ff1b1
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2026-06-16 16:27:51 -0600

    fix(cache): prevent web caching

--- src/e3sm_quickview/module/__init__.py
+++ src/e3sm_quickview/module/__init__.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 
+from e3sm_quickview import __version__
+
 __all__ = ["serve", "scripts"]
 
-serve = {"quick_view": str(Path(__file__).with_name("serve").resolve())}
+serve = {f"quick_view_{__version__}": str(Path(__file__).with_name("serve").resolve())}
 scripts = [
-    "quick_view/html2canvas.js",
-    "quick_view/utils.js",
+    f"quick_view_{__version__}/html2canvas.js",
+    f"quick_view_{__version__}/utils.js",
 ]
-styles = ["quick_view/style.css"]
+styles = [f"quick_view_{__version__}/style.css"]
